c7ecc8d909cd6b02add49223c41ad788025afbc1
Ivankov <ivan20481@gmail.com>
2020-02-08 12:26:22 +0300

    add utils

--- src/const.js
+++ src/const.js
@@ -1,1 +1,8 @@
 export const ARTICLES_COUNT = 5;
+
+export const OFFERS_NAME = [
+  `Beautiful &amp; luxurious apartment at great location`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`
+];
