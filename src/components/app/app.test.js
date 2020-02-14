cd4117f5a1f07080d47b3d8678c2524632c48232
Ivankov <ivan20481@gmail.com>
2020-02-14 20:05:04 +0300

    add folder mocks

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app.jsx";
-import {OFFERS_NAME_TEST} from "../../mock/test/data.js";
+import {OFFERS_NAME_TEST} from "../../mocks/test/data.js";
 
 it(`Render App`, () => {
   const tree = renderer
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Main from "./main.jsx";
-import {OFFERS_NAME_TEST} from "../../mock/test/data";
+import {OFFERS_NAME_TEST} from "../../mocks/test/data";
 
 it(`Render Main`, () => {
   const tree = renderer
