67138361313eff545d8eeb19aeb8a044eac66f6c
Ivankov <ivan20481@gmail.com>
2020-02-15 11:03:22 +0300

    add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    mark: true,
+    image: `img/apartment-01.jpg`,
+    price: 100,
+    bookmark: false,
+    rating: 4,
+    name: `luxurious apartment at great location`,
+    type: `Apartment`
+  },
+  {
+    mark: true,
+    image: `img/room.jpg`,
+    price: 123,
+    bookmark: true,
+    rating: 3,
+    name: `Wood and stone place`,
+    type: `Private room`
+  },
+  {
+    mark: false,
+    image: `img/apartment-02.jpg`,
+    price: 98,
+    bookmark: true,
+    rating: 2,
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`
+  },
+  {
+    mark: false,
+    image: `img/apartment-03.jpg`,
+    price: 131,
+    bookmark: false,
+    rating: 5,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`
+  }
+];
