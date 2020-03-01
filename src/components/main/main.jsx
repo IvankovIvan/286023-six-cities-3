7cc82e7fcede24dbff6c52cf367762470e8dc758
Ivankov <ivan20481@gmail.com>
2020-03-01 21:38:46 +0300

    add header

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -7,6 +7,7 @@ import OfferList from "../offer-list/offer-list.jsx";
 import Map from "../map/map.jsx";
 import Cities from "../cities/cities.jsx";
 import withActiveItem from "../../hocs/with-active-item/with-active-item.js";
+import Header from "../header/header.jsx";
 
 const OfferListWrapped = withOfferList(OfferList);
 const CitiesWrapped = withActiveItem(Cities);
@@ -18,32 +19,7 @@ const Main = (props) => {
   return (
     <div className="page page--gray page--main">
       <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg"
-                  alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile"
-                    href="#">
-                    <div
-                      className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span
-                      className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <Header isHref={false}/>
       </header>
 
       <main className="page__main page__main--index">
