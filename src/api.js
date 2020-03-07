f6ab4f7b868adada4ff483f0242e8f05d8e9d63a
Ivankov <ivan20481@gmail.com>
2020-03-07 08:42:13 +0300

    add api

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const TIMEOUT = 5000;
+
+export const createAPI = () => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/six-cities`,
+    timeout: TIMEOUT,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app.jsx";
 import {reducer} from "./reducer.js";
+import {createAPI} from "./api";
 
-const store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+const api = createAPI();
+
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ?
+          window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
 
 ReactDOM.render(
